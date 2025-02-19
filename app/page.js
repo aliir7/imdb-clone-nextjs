aa75b2b06602cb5410feb028cba8a4461e519d04
ALI <alirezaei.eng7@gmail.com>
2025-02-19 23:14:23 +0330

    add data

--- app/page.js
+++ app/page.js
@@ -1,5 +1,17 @@
 import Image from "next/image";
-
-export default function Home() {
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+export default async function Home() {
+  try {
+    const res = await fetch(
+      `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}`,
+    );
+    const data = await res.json();
+    console.log(data);
+    if (!res.ok) {
+      throw new Error("failed to fetch data");
+    }
+  } catch (error) {
+    console.log(error);
+  }
   return <div className="text-red-600">Home</div>;
 }
---
+++ components/Spinner.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div className="h-20 w-20 animate-spin rounded-full border-8 border-gray-900 border-t-transparent"></div>
+  );
+};
+
+export default Spinner;
